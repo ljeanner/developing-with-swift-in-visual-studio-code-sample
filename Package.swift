ebe63f97a308c54f3cb2605c29a044a41d4f0997
Lucile Jeanneret <lujeanne@microsoft.com>
2024-12-20 01:35:40 +0100

    Add vulnerable dependency

--- Package.swift
+++ Package.swift
@@ -4,11 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "sample-project",
+    name: "ios-project",
+     dependencies: [
+        // Add a vulnerable version of a dependency
+        .package(url: "https://github.com/vulnerable/library.git", from: "1.0.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "sample-project"),
+            name: "ios-project"),
     ]
 )
